f8cb7ebef7e5d489996bd1826b2845668779c0aa
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-17 15:51:47 -0500

    Add UI for game

--- script.js
+++ script.js
@@ -47,98 +47,114 @@ let playerScore = 0;
 // Create computerScore variable
 let computerScore = 0;
 
-// Create determineWinner function with two parameters (playerSelection and computerSelection) to determine winner of round
-function determineWinner(playerSelection, computerSelection) {
-// Create a tie variable
-let tie = "It's a tie!"
-// Create a winner variable
-let playerWon = "You Win!"
-// Create a loser variable
-let playerLost = "You Lose!"
-// If playerSelection = computerSelection then return "It's a tie!"
-if (playerSelection === computerSelection) {
-    return tie;
-// If computerSelection = "Rock" and playerSelection = "Paper" then return "You Win! Paper beats Rock"
-} else if (computerSelection === "Rock") {
-    if (playerSelection === "Paper") {
-        ++playerScore;
-        return playerWon + " Paper beats Rock";
-// If computerSelection = "Rock" and playerSelection = "Scissors" then return "You Lose! Rock beats Scissors"
-    } else {
-        ++computerScore;
-        return playerLost + " Rock beats Scissors"
-    }
-// If computerSelection = "Paper" and playerSelection = "Scissors" then return "You Win! Scissors beats Paper"
-} else if (computerSelection === "Paper") {
-    if (playerSelection === "Scissors") {
-        ++playerScore;
-        return playerWon + " Scissors beats Rock";
-// If computerSelection = "Paper" and playerSelection = "Rock" then return "You Lose! Paper beats Rock"
-    } else {
-        ++computerScore;
-        return playerLost + " Paper beats Rock"
-    }
-// If computerSelection = "Scissors" and playerSelection = "Paper" then return "You Lose! Scissors beats Paper"
-} else if (computerSelection === "Scissors") {
-    if (playerSelection === "Paper") {
-        ++computerScore;
-        return playerLost + " Scissors beats Paper";
+// Create playRound function with two parameters (playerSelection and computerSelection) to determine winner of round
+function playRound(playerSelection, computerSelection) {
+    // Create a tie variable
+    let tie = "It's a tie!"
+    // Create a winner variable
+    let playerWon = "You Win!"
+    // Create a loser variable
+    let playerLost = "You Lose!"
+    // If playerSelection = computerSelection then return "It's a tie!"
+    if (playerSelection === computerSelection) {
+        return tie;
+    // If playerSelection = "Rock" and computerSelection = "Paper" then return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            ++computerScore;
+            return playerLost + " Paper beats Rock";
+    // If playerSelection = "Rock" and computerSelection = "Scissors" then return "You Win! Rock beats Scissors"
+        } else {
+            ++playerScore;
+            return playerWon + " Rock beats Scissors"
+        }
+    // If playerSelection = "Paper" and computerSelection = "Scissors" then return "You Lose! Scissors beats Paper"
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Scissors") {
+            ++computerScore;
+            return playerLost + " Scissors beats Rock";
+    // If playerSelection = "Paper" and computerSelection = "Rock" then return "You Win! Paper beats Rock"
+        } else {
+            ++playerScore;
+            return playerWon + " Paper beats Rock"
+        }
+    // If playerSelection = "Scissors" and computerSelection = "Paper" then return "You Win! Scissors beats Paper"
+    } else if (playerSelection === "Scissors") {
+        if (computerSelection === "Paper") {
+            ++playerScore;
+            return playerWon + " Scissors beats Paper";
 
-// If computerSelection = "Scissors" and playerSelection = "Rock" then return "You Win! Rock beats Scissors"
+    // If playerSelection = "Scissors" and computerSelection = "Rock" then return "You Lose! Rock beats Scissors"
+        } else {
+            ++computerScore;
+            return playerLost + " Rock beats Scissors"
+        }
+    // Return error if conditions are not met
     } else {
-        ++playerScore;
-        return playerWon + " Rock beats Scissors"
-    }
-// Return error if conditions are not met
-} else {
-    return "ERROR"
-};
-
+        return "ERROR"
+    };
 }
-
-//console.log(determineWinner(playerSelection, computerChoice));
 
 // Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end.
 
-
 // Create function called game
-function game() {
-// Call the determineWinner function
-getComputerChoice();
-getPlayerChoice();
-console.log(determineWinner(playerSelection, computerChoice));
-console.log("The computer score is " + computerScore);
-console.log("The player score is " + playerScore);
-getComputerChoice();
-getPlayerChoice();
-console.log(determineWinner(playerSelection, computerChoice));
-console.log("The computer score is " + computerScore);
-console.log("The player score is " + playerScore);
-getComputerChoice();
-getPlayerChoice();
-console.log(determineWinner(playerSelection, computerChoice));
-console.log("The computer score is " + computerScore);
-console.log("The player score is " + playerScore);
-getComputerChoice();
-getPlayerChoice();
-console.log(determineWinner(playerSelection, computerChoice));
-console.log("The computer score is " + computerScore);
-console.log("The player score is " + playerScore);
-getComputerChoice();
-getPlayerChoice();
-console.log(determineWinner(playerSelection, computerChoice));
-console.log("The computer score is " + computerScore);
-console.log("The player score is " + playerScore);
-// Console log the winner of the game
+function game(playerSelection) {
+    getComputerChoice();
 
-if (computerScore > playerScore) {
-    console.log("You lost the game!");
-} else if (computerScore < playerScore) {
-    console.log("You won the game!");
-} else {
-    console.log("It's a tie!");
-}
+    function empty(element) {
+        element.replaceChildren(); 
+     }
+     
+     let parent = document.getElementById("results");
+     empty(parent);
+
+    // Add paragraph showing round being played
+    const roundPara = document.createElement("p");
+    const roundNode = document.createTextNode(playRound(playerSelection, computerChoice));
+    roundPara.appendChild(roundNode);
+    const element = document.getElementById("results");
+    element.appendChild(roundPara);
+
+    // Add paragraph showing computer score
+    const computerPara = document.createElement("p");
+    const computerNode = document.createTextNode("The computer score is " + computerScore);
+    computerPara.appendChild(computerNode);
+    element.appendChild(computerPara);
+
+    // Add paragraph showing player score
+    const playerPara = document.createElement("p");
+    const playerNode = document.createTextNode("The player score is " + playerScore);
+    playerPara.appendChild(playerNode);
+    element.appendChild(playerPara);
+
+    // Show the winner of the game
+    if (computerScore === 5 || playerScore === 5) {
+        if (computerScore > playerScore) {
+            // Add paragraph showing you lost
+            const playerLostPara = document.createElement("p");
+            const playerLostNode = document.createTextNode("You lost the game!");
+            playerLostPara.appendChild(playerLostNode);
+            element.appendChild(playerLostPara);
+
+        } else if (computerScore < playerScore) {
+            // Add paragraph showing you won
+            const playerWonPara = document.createElement("p");
+            const playerWonNode = document.createTextNode("You won the game!");
+            playerWonPara.appendChild(playerWonNode);
+            element.appendChild(playerWonPara);
+        } else {
+            // Add paragraph showing it's a tie.
+            const tiePara = document.createElement("p");
+            const tieNode = document.createTextNode("It's a tie!");
+            tiePara.appendChild(tieNode);
+            element.appendChild(tiePara);
+        }
+    }
+
 }
 
-// Call game function
-game();
+// Create event listeners for each button that call your playRound function with the correct playerSelection every time a button is clicked.
+buttonRock.addEventListener('click', game.bind (null, "Rock"));
+buttonPaper.addEventListener('click', game.bind (null, "Paper"));
+buttonScissors.addEventListener('click', game.bind (null, "Scissors"));
+
